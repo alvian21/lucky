ed10dc9b02bd7107934fe2a12d14fb4622417164
alvian <alvian.ardhiansyah@gmail.com>
2020-01-08 23:56:54 +0700

    pengeluaran

--- app/Http/Controllers/Dashboard/PengeluaranController.php
+++ app/Http/Controllers/Dashboard/PengeluaranController.php
@@ -7,5 +7,8 @@ use Illuminate\Http\Request;
 
 class PengeluaranController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('dashboard.pengeluaran.index');
+    }
 }

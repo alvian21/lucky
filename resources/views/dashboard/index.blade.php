ffdb33a3d5474306fc50c88128a87c243deda82a
alvian <alvian.ardhiansyah@gmail.com>
2020-01-13 20:48:23 +0700

    date

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -27,161 +27,41 @@
     </div>
 </div>
 <div class="row">
-    <div class="col-md-6">
-        <div class="card ">
+
+    <div class="col-md-12">
+        <div class="card strpied-tabled-with-hover">
             <div class="card-header ">
-                <h4 class="card-title">2017 Sales</h4>
-                <p class="card-category">All products including Taxes</p>
-            </div>
-            <div class="card-body ">
-                <div id="chartActivity" class="ct-chart"></div>
-            </div>
-            <div class="card-footer ">
-                <div class="legend">
-                    <i class="fa fa-circle text-info"></i> Tesla Model S
-                    <i class="fa fa-circle text-danger"></i> BMW 5 Series
+                <div class="row">
+                    <div class="col-md-2">
+                            <h4 class="text-black">Data</h4>
+                    </div>
+
                 </div>
-                <hr>
-                <div class="stats">
-                    <i class="fa fa-check"></i> Data information certified
-                </div>
+
             </div>
-        </div>
-    </div>
-    <div class="col-md-6">
-        <div class="card  card-tasks">
-            <div class="card-header ">
-                <h4 class="card-title">Tasks</h4>
-                <p class="card-category">Backend development</p>
-            </div>
-            <div class="card-body ">
-                <div class="table-full-width">
-                    <table class="table">
-                        <tbody>
-                            <tr>
-                                <td>
-                                    <div class="form-check">
-                                        <label class="form-check-label">
-                                            <input class="form-check-input" type="checkbox" value="">
-                                            <span class="form-check-sign"></span>
-                                        </label>
-                                    </div>
-                                </td>
-                                <td>Sign contract for "What are conference organizers afraid of?"</td>
-                                <td class="td-actions text-right">
-                                    <button type="button" rel="tooltip" title="Edit Task" class="btn btn-info btn-simple btn-link">
-                                        <i class="fa fa-edit"></i>
-                                    </button>
-                                    <button type="button" rel="tooltip" title="Remove" class="btn btn-danger btn-simple btn-link">
-                                        <i class="fa fa-times"></i>
-                                    </button>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <div class="form-check">
-                                        <label class="form-check-label">
-                                            <input class="form-check-input" type="checkbox" value="" checked>
-                                            <span class="form-check-sign"></span>
-                                        </label>
-                                    </div>
-                                </td>
-                                <td>Lines From Great Russian Literature? Or E-mails From My Boss?</td>
-                                <td class="td-actions text-right">
-                                    <button type="button" rel="tooltip" title="Edit Task" class="btn btn-info btn-simple btn-link">
-                                        <i class="fa fa-edit"></i>
-                                    </button>
-                                    <button type="button" rel="tooltip" title="Remove" class="btn btn-danger btn-simple btn-link">
-                                        <i class="fa fa-times"></i>
-                                    </button>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <div class="form-check">
-                                        <label class="form-check-label">
-                                            <input class="form-check-input" type="checkbox" value="" checked>
-                                            <span class="form-check-sign"></span>
-                                        </label>
-                                    </div>
-                                </td>
-                                <td>Flooded: One year later, assessing what was lost and what was found when a ravaging rain swept through metro Detroit
-                                </td>
-                                <td class="td-actions text-right">
-                                    <button type="button" rel="tooltip" title="Edit Task" class="btn btn-info btn-simple btn-link">
-                                        <i class="fa fa-edit"></i>
-                                    </button>
-                                    <button type="button" rel="tooltip" title="Remove" class="btn btn-danger btn-simple btn-link">
-                                        <i class="fa fa-times"></i>
-                                    </button>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <div class="form-check">
-                                        <label class="form-check-label">
-                                            <input class="form-check-input" type="checkbox" checked>
-                                            <span class="form-check-sign"></span>
-                                        </label>
-                                    </div>
-                                </td>
-                                <td>Create 4 Invisible User Experiences you Never Knew About</td>
-                                <td class="td-actions text-right">
-                                    <button type="button" rel="tooltip" title="Edit Task" class="btn btn-info btn-simple btn-link">
-                                        <i class="fa fa-edit"></i>
-                                    </button>
-                                    <button type="button" rel="tooltip" title="Remove" class="btn btn-danger btn-simple btn-link">
-                                        <i class="fa fa-times"></i>
-                                    </button>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <div class="form-check">
-                                        <label class="form-check-label">
-                                            <input class="form-check-input" type="checkbox" value="">
-                                            <span class="form-check-sign"></span>
-                                        </label>
-                                    </div>
-                                </td>
-                                <td>Read "Following makes Medium better"</td>
-                                <td class="td-actions text-right">
-                                    <button type="button" rel="tooltip" title="Edit Task" class="btn btn-info btn-simple btn-link">
-                                        <i class="fa fa-edit"></i>
-                                    </button>
-                                    <button type="button" rel="tooltip" title="Remove" class="btn btn-danger btn-simple btn-link">
-                                        <i class="fa fa-times"></i>
-                                    </button>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <div class="form-check">
-                                        <label class="form-check-label">
-                                            <input class="form-check-input" type="checkbox" value="" disabled>
-                                            <span class="form-check-sign"></span>
-                                        </label>
-                                    </div>
-                                </td>
-                                <td>Unfollow 5 enemies from twitter</td>
-                                <td class="td-actions text-right">
-                                    <button type="button" rel="tooltip" title="Edit Task" class="btn btn-info btn-simple btn-link">
-                                        <i class="fa fa-edit"></i>
-                                    </button>
-                                    <button type="button" rel="tooltip" title="Remove" class="btn btn-danger btn-simple btn-link">
-                                        <i class="fa fa-times"></i>
-                                    </button>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-            <div class="card-footer ">
-                <hr>
-                <div class="stats">
-                    <i class="now-ui-icons loader_refresh spin"></i> Updated 3 minutes ago
-                </div>
+            <div class="card-body table-full-width table-responsive">
+                <table class="table table-hover table-striped">
+                    <thead>
+                        <tr><th>kode</th>
+                        <th>Name</th>
+                        <th>Jumlah</th>
+                        <th>Tipe</th>
+                    </tr></thead>
+                    <tbody>
+                        @foreach($transaction as $row)
+                       <tr>
+                       <td>{{$row['kode']}}</td>
+                        <td>{{ $row['name'] }}</td>
+                        <td>{{ $row['price'] }}</td>
+                         @if($row['type'] == 'Pemasukan')
+                            <td> <span class="fa fa-circle text-info"></span> Pemasukan</td>
+                            @else
+                            <td><span class="fa fa-circle text-danger"></span>Pengeluaran</td>
+                            @endif
+                       </tr>
+                       @endforeach
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
